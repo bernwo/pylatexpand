8654aed80a1e5ffb52ab6aa1189efed55b2d1cc3
bernwo <53580103+bernwo@users.noreply.github.com>
2023-10-15 12:30:37 +0800

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 setup(
     name="pylatexpand",
-    python_requires="~=3.11",
+    python_requires=">=3.10.3",
     version="1.0",
     packages=["pylatexpand"],
-    install_requires=[],
     entry_points={
         "console_scripts": ["pylatexpand=bin:main"],
     },
